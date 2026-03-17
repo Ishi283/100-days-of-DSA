e21387bc6ddfbc10a83c6c44b58bc0b8a0f54699
Ishi283 <ishiagarwal2829@gmail.com>
2026-03-17 23:08:07 +0530

    Add files via upload

---
+++ day11.c
@@ -0,0 +1,106 @@
+/*Problem: Write a program to perform addition of two matrices having the same dimensions. The sum of two matrices is obtained by adding corresponding elements of the matrices.
+
+Input:
+- First line: two integers m and n representing the number of rows and columns
+- Next m lines: n integers each representing the elements of the first matrix
+- Next m lines: n integers each representing the elements of the second matrix
+
+Output:
+- Print the resultant matrix after addition, with each row on a new line and elements separated by spaces
+
+Example:
+Input:
+3 3
+1 2 3
+4 5 6
+7 8 9
+9 8 7
+6 5 4
+3 2 1
+
+Output:
+10 10 10
+10 10 10
+10 10 10
+
+Explanation:
+Each element of the result matrix is the sum of the corresponding elements from the two input matrices.
+
+Test Cases:
+
+Test Case 1:
+Input:
+2 2
+1 2
+3 4
+5 6
+7 8
+Output:
+6 8
+10 12
+
+Test Case 2:
+Input:
+1 3
+10 20 30
+1 2 3
+Output:
+11 22 33
+
+Test Case 3:
+Input:
+3 1
+1
+2
+3
+4
+5
+6
+Output:
+5
+7
+9
+*/
+#include<stdio.h>
+int main()
+{
+    int n1,n2;
+    printf("Enter the size of array 1 and array 2:");
+    scanf("%d %d",&n1,&n2);
+    int arr1[n1][n2],arr2[n1][n2];
+    printf("enter the elements of 1st array:");
+    for(int i=0;i<n1;i++)
+    {
+        for(int j=0;j<n2;j++)
+        {
+        scanf("%d",&arr1[i][j]);
+        }
+    }
+    printf("Enter the elemnts of 2nd array:");
+    for(int i=0;i<n1;i++)
+    {
+         for(int j=0;j<n2;j++)
+         {
+        scanf("%d",&arr2[i][j]);
+         }
+    }
+    int sum[n1][n2];
+    for(int i=0;i<n1;i++)
+    {
+        for(int j=0;j<n2;j++)
+        {
+            sum[i][j] = arr1[i][j] + arr2[i][j];
+        }
+    }
+    printf("sum :");
+    for(int i=0;i<n1;i++)
+    {
+        for(int j=0;j<n2;j++)
+        {
+            printf("%d ",sum[i][j]);
+        }
+        
+    }
+    printf("\n");
+    return 0;
+}
